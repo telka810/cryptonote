e315438bb7be56ea8cc8fd470f1b9971ec441aa9
telka810 <djnaps007@hotmail.com>
2017-11-29 22:19:24 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "62.210.105.201:28080"
+  "62.210.105.201:28080",
   //"your_seed_ip2.com:8080",
 };
 
